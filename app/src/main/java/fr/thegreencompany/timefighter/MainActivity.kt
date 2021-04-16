fd11fd7cb174950d1cf82ae8e746cf500190685f
Sebastien REMY <seb.remy@me.com>
2021-04-16 19:31:11 +0200

    Add a menu

--- app/src/main/java/fr/thegreencompany/timefighter/MainActivity.kt
+++ app/src/main/java/fr/thegreencompany/timefighter/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.PersistableBundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -55,6 +57,23 @@ class MainActivity : AppCompatActivity() {
         }
 
         Log.d(TAG, "onCreate score is: $score")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.actionAbout) {
+            showInfo()
+        }
+        return true
+    }
+
+    private fun showInfo() {
+        
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
